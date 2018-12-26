e64d082455e36347b331bd0a3eab03e0630618e4
neverhooda <neverhooda@gmail.com>
2018-12-26 17:55:14 +0700

    update & test on yorick, success

--- yorick.py
+++ yorick.py
@@ -4,6 +4,11 @@
 import cherrypy
 from os.path import abspath
 
+import subprocess
+from os import listdir
+from os.path import isfile, join
+from random import randint
+
 
 class Yorick(object):
     """ Service for control Yorick"""
@@ -16,6 +21,20 @@ class Yorick(object):
     def greet(self, name):
         return 'Hello {}!'.format(name)
 
+    @cherrypy.expose
+    def play_random(self):
+        print("hello")
+        dir_t = "/home/gress/work/terminator"
+        # dir_t = "C:/Users/ank/Downloads/Telegram Desktop"
+        only_files = [f for f in listdir(dir_t) if isfile(join(dir_t, f))]
+        mp3_files = []
+        for mp in only_files:
+            if mp.endswith('.mp3'):
+                mp3_files.append(mp)
+        subprocess.check_call(["mpg123", "%s/%s" % (dir_t, mp3_files[randint(0, len(mp3_files))])])
+        return mp3_files
+        # return '{"status":200}'
+
 
 configuration = {
             '/': {
@@ -24,5 +43,8 @@ configuration = {
                 },
 }
 
+cherrypy.config.update({'server.socket_host': '64.72.221.48',
+                        'server.socket_port': 80,
+                       })
 
 cherrypy.quickstart(Yorick(), '/', configuration)
